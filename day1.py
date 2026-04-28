11a6e83e44b151358e9f68296e5352df00f5e370
reshmabodaballa-art <reshmabodaballa@gmail.com>
2026-04-28 10:38:43 +0530

    Add files via upload
    day1- Introdution of python

---
+++ day1.py
@@ -0,0 +1,37 @@
+'''print("This is my first day at codegnan")
+what is python?
+--> Python is a high-level-language means no need to worry about low-level management like memory allocation.
+'''
+'''
+-->Python never save anything permanently, it will onlysave at run time
+'''
+a = 0
+b = 6
+print(id(a))
+
+'''
+--> Python supports object oriented language
+'''
+
+a = int(input("enter a num: "))
+b = int(input("enter a num: "))
+print(f"b = (b)")
+print(f"a = (a)")
+
+'''
+-->This is also called dynamically-typed language no need to mention the type of data your take in a variable---
+'''
+a = 8
+b = "sarayu"
+(type(a))
+print(type(b))
+
+'''
+-->Python is called interpreted language means the python code will execute line by line
+'''
+a = 5
+b = 5
+print(a+b)
+'''
+-->syntax less language
+'''
